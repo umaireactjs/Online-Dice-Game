c7fd9b97e2aa2e68b6efbbf7c832d78dd50516d9
umair <umairqamar500@gmail.com>
2023-09-09 15:49:52 +0500

    fix: dice_.png

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -5,7 +5,7 @@ const StartGame = ({ toggle }) => {
   return (
     <Container>
       <div>
-        <img src="./images/dices.png" className="new" alt= "agd"  />
+        <img src="./images/dices.png" className="new"/>
       </div>
       <div className="content">
         <h1>Dice Game</h1>
